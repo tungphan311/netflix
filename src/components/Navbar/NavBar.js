36513e2b3329431aea2ce249b06842155d0a019f
unknown <thanhtunga1lqd@gmail.com>
2019-11-15 14:58:23 +0700

    finish navigator

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,15 +1,51 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/anchor-has-content */
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.scss";
+import OutsideClickWrapper from "../OutsideClickWrapper/OutsideClickWrapper";
 import NavigatorItem from "./NavigatorItem/NavigatorItem";
 
 function NavBar({ route }) {
+  const [show, setShow] = useState(false);
+  const display = show ? "navbar__sub-navigator__show" : "d-none";
   return (
     <div className="navbar__container">
       <a aria-label="Netflix" className="navbar_logo" href="/" />
       <ul className="navbar__navigator">
+        <li className="navbar__navigator__selector">
+          <OutsideClickWrapper
+            onClickOutside={() => setShow(false)}
+            isShowing={show}
+          >
+            <a
+              className="menu-trigger"
+              role="button"
+              tabIndex="0"
+              onClick={() => setShow(!show)}
+            >
+              Browser
+            </a>
+
+            <div className={`navbar__sub-navigator ${display}`}>
+              <div className="callout-arrow"></div>
+              <div className="topbar"></div>
+              <ul className="navbar__sub-navigator__menu">
+                {NAV_ITEMS.map(({ title, href }) => (
+                  <NavigatorItem
+                    subItem
+                    key={title}
+                    title={title}
+                    href={href}
+                    isActive={route === href}
+                  />
+                ))}
+              </ul>
+            </div>
+          </OutsideClickWrapper>
+        </li>
         {NAV_ITEMS.map(({ title, href }) => (
           <NavigatorItem
+            subItem={false}
             key={title}
             title={title}
             href={href}
--- src/components/Navbar/NavigatorItem/NavigatorItem.js
+++ src/components/Navbar/NavigatorItem/NavigatorItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./NavigatorItem.scss";
 
-function NavigatorItem({ title, isActive, href }) {
-  const className = isActive ? "active" : "";
+function NavigatorItem({ title, isActive, href, subItem }) {
+  const liClassName = subItem
+    ? "navbar__sub-navigator__item"
+    : "navbar__navigator__item";
+
+  let aClassName = subItem ? "sub-menu__item" : "menu__item";
+  aClassName = isActive ? `${aClassName} active` : aClassName;
 
   return (
-    <li className="navbar__navigator__item">
-      <a className={className} href={href}>
+    <li className={liClassName}>
+      <a className={aClassName} href={href}>
         {title}
       </a>
     </li>
