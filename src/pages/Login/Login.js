7874f80ea3f6965be6fed8036d3b371a13a7ca88
Nguyễn Đăng Quang <quangnd@1ci.vn>
2019-11-23 14:35:38 +0700

    Update Login Form

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -15,54 +15,57 @@ function Login(props) {
 
   return (
     <div className="Login">
-      <form onSubmit={handleSubmit}>
-        <div className="Background" />
-        <Form>
-          <Jumbotron>
-            <h3>Welcome back! </h3>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Control
-                autoFocus
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-              />
-            </Form.Group>
+      <div className="form__overlay" />
+      <div className="form__container">
+        <form onSubmit={handleSubmit}>
+          <div className="Background" />
+          <Form>
+            <Jumbotron>
+              <h1>Welcome back! </h1>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Control
+                  autoFocus
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                />
+              </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-              {/* <Form.Check type="checkbox" label="Keep me logged in" /> */}
-              <Form.Check
-                type="switch"
-                id="custom-switch"
-                label="Keep me logged in"
-              />
-            </Form.Group>
-            <Form.Control
-              size="sm"
-              plaintext
-              readOnly
-              defaultValue="First time? Click here"
-            />
-            <Button
-              variant="primary"
-              type="submit"
-              disabled={!validateForm()}
-              block
-            >
-              Login
-            </Button>
-          </Jumbotron>
-        </Form>
-      </form>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Button type="submit" block>
+                Login
+              </Button>
+              <Form.Group controlId="formBasicCheckbox" bsPrefix="helper">
+                {/* <Form.Check type="checkbox" label="Keep me logged in" /> */}
+                <Form.Check
+                  type="switch"
+                  id="custom-switch"
+                  label="Remember me"
+                />
+
+                <a className="forgotLink" href="/forgot-password">
+                  Need help?
+                </a>
+              </Form.Group>
+
+              <div className="register">
+                First time?{" "}
+                <span className="registerLink">
+                  <a href="/register">Sign up now</a>
+                </span>
+              </div>
+            </Jumbotron>
+          </Form>
+        </form>
+      </div>
     </div>
   );
 }
