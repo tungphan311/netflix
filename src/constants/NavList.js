4c07557e8a2efab705ccc1f0136445357248fb28
tungphan311 <thanhtunga1lqd@gmail.com>
2020-01-10 23:49:05 +0700

    navbar

--- src/constants/NavList.js
+++ src/constants/NavList.js
@@ -1,8 +1,8 @@
 export const NAV_ITEMS = [
   { title: "Home", href: "/" },
-  { title: "TV Shows", href: "#" },
-  { title: "Movies", href: "#" },
-  { title: "My favorites", href: "#" }
+  { title: "TV Shows", href: "/browse?type=tv%20show" },
+  { title: "Movies", href: "/browse?type=movie" },
+  { title: "My favorites", href: "/my-favorites" }
 ];
 
 export const RIGHT_NAV_ITEMS = [
